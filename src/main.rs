5327b10acf7fadd9cf31f00a07cda9ec136be538
Štěpán Beneš <benes.stepan@gmail.com>
2020-09-23 22:49:12 +0200

    Add more links

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ fn main() {
 
     // TODO: https://rust-lang-nursery.github.io/rust-cookbook/file/dir.html
 
+    // https://raspberrypi.stackexchange.com/questions/8295/external-hard-drive-power-down
+    // https://www.raspberrypi.org/documentation/configuration/external-storage.md
+
 }
