b024608e125ca0e621b003386feab70f2e7e6fb4
Štěpán Beneš <benes.stepan@gmail.com>
2020-09-24 14:18:10 +0200

    Add walkdir crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use walkdir::WalkDir;
+
 fn main() {
     println!("Hello, I am memory keeper! Your personal data backup service.");
+
+    let source_location = "/mnt/blob";
+    let _backup_location = "/mnt/bloback";
+
+    let _sync_selection = vec!["hana", "stepan", "hovada", "test/data_sync"];
+
+    let total_size = WalkDir::new(source_location)
+        // .min_depth(1)
+        // .max_depth(3)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter_map(|entry| entry.metadata().ok())
+        .filter(|metadata| metadata.is_file())
+        .fold(0, |acc, m| acc + m.len());
+
+    println!("Total size: {} bytes.", total_size);
 
     // TODO: https://rust-lang-nursery.github.io/rust-cookbook/file/dir.html
 
