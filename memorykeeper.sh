b98acfb2a2acf996694cabbcdfa3c1a68dfc7543
Stepan Benes <benes.stepan@gmail.com>
2020-09-30 18:48:24 +0200

    Add memorykeeper script

---
+++ memorykeeper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATE_STR=$(date +"%Y-%m-%d")
+BRANCH_NAME=$(date +"auto-backup/%Y/%m/%d")
+
+# run backup script
+sudo ./backup.sh "$BRANCH_NAME" 2>&1 | sudo tee -a "/var/log/memorykeeper/$DATE_STR.log"  # append output of backup script to a log file
+
+# Power down external HDD
+# see: https://raspberrypi.stackexchange.com/questions/8295/external-hard-drive-power-down
+sudo hdparm -y /dev/sda1
