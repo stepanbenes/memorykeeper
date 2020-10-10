42398e725c298a7f0197af26e9958f868f2b3d66
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-10 23:53:55 +0200

    Fix path to backup.sh

--- memorykeeper.sh
+++ memorykeeper.sh
@@ -4,7 +4,7 @@ DATE_STR=$(date +"%Y-%m-%d")
 BRANCH_NAME=$(date +"auto-backup/%Y/%m/%d")
 
 # run backup script
-sudo ./backup.sh "$BRANCH_NAME" 2>&1 | sudo tee -a "/var/log/memorykeeper/$DATE_STR.log"  # append output of backup script to a log file
+sudo /home/pi/projects/memorykeeper/backup.sh "$BRANCH_NAME" 2>&1 | sudo tee -a "/var/log/memorykeeper/$DATE_STR.log"  # append output of backup script to a log file
 
 # Power down external HDD
 # see: https://raspberrypi.stackexchange.com/questions/8295/external-hard-drive-power-down
