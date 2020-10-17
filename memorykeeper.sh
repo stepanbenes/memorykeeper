a364e02f1adfe7dedbedf65dc77c3e87e8a155a9
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-18 01:03:04 +0200

    Write backup fail indication to log

--- memorykeeper.sh
+++ memorykeeper.sh
@@ -2,8 +2,18 @@
 
 DATE_STR=$(date +"%Y-%m-%d")
 
+LOG_FILE_PATH="/var/log/memorykeeper"
+
 # run backup script
-sudo /home/pi/projects/memorykeeper/backup.sh 2>&1 | sudo tee -a "/var/log/memorykeeper/$DATE_STR.log"  # append output of backup script to a log file
+sudo /home/pi/projects/memorykeeper/backup.sh 2>&1 | sudo tee -a "$LOG_FILE_PATH/${DATE_STR}.log" # append output of backup script to a log file
+
+# append FAIL suffix to log file name
+if [ "${PIPESTATUS[0]}" -ne "0" ]
+then
+    echo -e '\nBACKUP FAILED!\n' | sudo tee -a "$LOG_FILE_PATH/${DATE_STR}.log"
+    #sudo mv "$LOG_FILE_PATH/${DATE_STR}.log" "$LOG_FILE_PATH/${DATE_STR}-FAIL.log" 
+fi
+
 
 # Power down external HDD
 # see: https://raspberrypi.stackexchange.com/questions/8295/external-hard-drive-power-down
