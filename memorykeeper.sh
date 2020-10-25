c07e1929dbd9cc13cacc5119dee1c8cdd0a14de1
Štěpán Beneš <benes.stepan@gmail.com>
2020-10-25 13:50:06 +0100

    Improve comment

--- memorykeeper.sh
+++ memorykeeper.sh
@@ -7,11 +7,10 @@ LOG_FILE_PATH="/var/log/memorykeeper"
 # run backup script
 sudo /home/pi/projects/memorykeeper/backup.sh 2>&1 | sudo tee -a "$LOG_FILE_PATH/${DATE_STR}.log" # append output of backup script to a log file
 
-# append FAIL suffix to log file name
+# append FAIL info to log file
 if [ "${PIPESTATUS[0]}" -ne "0" ]
 then
     echo -e '\nBACKUP FAILED!\n' | sudo tee -a "$LOG_FILE_PATH/${DATE_STR}.log"
-    #sudo mv "$LOG_FILE_PATH/${DATE_STR}.log" "$LOG_FILE_PATH/${DATE_STR}-FAIL.log" 
 fi
 
 
